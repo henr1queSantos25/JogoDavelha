ba176bbb89ea8c1a31d972150a79da5d92da922b
Henrique O. Santos <202410486@uesb.edu.br>
2024-12-18 19:45:29 -0300

    mudança de layout

--- src/Visual/PanelTabela.java
+++ src/Visual/PanelTabela.java
@@ -11,6 +11,9 @@ import javax.swing.SwingConstants;
 import javax.swing.border.BevelBorder;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class PanelTabela extends JPanel {
 
@@ -31,21 +34,65 @@ public class PanelTabela extends JPanel {
 
     
     public PanelTabela() {
-        setLayout(null);
-        this.add(getBotao0x0());
-        this.add(getBotao0x1());
-        this.add(getBotao0x2());
-        this.add(getBotao1x0());
-        this.add(getBotao1x1());
-        this.add(getBotao1x2());
-        this.add(getBotao2x0());
-        this.add(getBotao2x1());
-        this.add(getBotao2x2());
-
-        this.add(getJogarDeNovo());
-
-        this.add(getResultado());
-        this.add(getVezJogador());
+        GroupLayout groupLayout = new GroupLayout(this);
+        groupLayout.setHorizontalGroup(
+        	groupLayout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(groupLayout.createSequentialGroup()
+        			.addGap(154)
+        			.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+        				.addComponent(getVezJogador(), GroupLayout.DEFAULT_SIZE, 483, Short.MAX_VALUE)
+        				.addComponent(getJogarDeNovo(), GroupLayout.DEFAULT_SIZE, 483, Short.MAX_VALUE)
+        				.addGroup(groupLayout.createSequentialGroup()
+        					.addComponent(getBotao0x0(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+        					.addGap(32)
+        					.addComponent(getBotao0x1(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+        					.addGap(31)
+        					.addComponent(getBotao0x2(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE))
+        				.addGroup(groupLayout.createSequentialGroup()
+        					.addComponent(getBotao1x0(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+        					.addGap(32)
+        					.addComponent(getBotao1x1(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+        					.addGap(31)
+        					.addComponent(getBotao1x2(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE))
+        				.addGroup(groupLayout.createSequentialGroup()
+        					.addComponent(getBotao2x0(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+        					.addGap(32)
+        					.addComponent(getBotao2x1(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+        					.addGap(31)
+        					.addComponent(getBotao2x2(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE))
+        				.addComponent(getResultado(), GroupLayout.DEFAULT_SIZE, 483, Short.MAX_VALUE))
+        			.addGap(163))
+        );
+        groupLayout.setVerticalGroup(
+        	groupLayout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(groupLayout.createSequentialGroup()
+        			.addGap(31)
+        			.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+        				.addGroup(groupLayout.createSequentialGroup()
+        					.addGap(2)
+        					.addComponent(getVezJogador(), GroupLayout.DEFAULT_SIZE, 92, Short.MAX_VALUE)
+        					.addGap(13))
+        				.addComponent(getJogarDeNovo(), GroupLayout.DEFAULT_SIZE, 107, Short.MAX_VALUE))
+        			.addGap(32)
+        			.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+        				.addComponent(getBotao0x0(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+        				.addComponent(getBotao0x1(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+        				.addComponent(getBotao0x2(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE))
+        			.addGap(18)
+        			.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+        				.addComponent(getBotao1x0(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+        				.addComponent(getBotao1x1(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+        				.addComponent(getBotao1x2(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE))
+        			.addGap(23)
+        			.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+        				.addComponent(getBotao2x0(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+        				.addComponent(getBotao2x1(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+        				.addComponent(getBotao2x2(), GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE))
+        			.addGap(44)
+        			.addComponent(getResultado(), GroupLayout.DEFAULT_SIZE, 66, Short.MAX_VALUE)
+        			.addGap(59))
+        );
+        setLayout(groupLayout);
 
     }
 
@@ -55,8 +102,6 @@ public class PanelTabela extends JPanel {
             vezJogador.setForeground(Color.BLACK);
             vezJogador.setHorizontalAlignment(SwingConstants.CENTER);
             vezJogador.setFont(new Font("Comic Sans MS", Font.PLAIN, 45));
-            vezJogador.setSize(483, 92);
-            vezJogador.setLocation(118, 27);
             vezJogador.setText("Vez do Jogador 1(X)");
         }
         return vezJogador;
@@ -68,8 +113,6 @@ public class PanelTabela extends JPanel {
             resultado.setForeground(Color.BLACK);
             resultado.setHorizontalAlignment(SwingConstants.CENTER);
             resultado.setFont(new Font("Comic Sans MS", Font.PLAIN, 45));
-            resultado.setSize(483, 92);
-            resultado.setLocation(118, 635);
             resultado.setText("");
         }
         return resultado;
@@ -88,8 +131,6 @@ public class PanelTabela extends JPanel {
             });
             jogarDeNovo.setVisible(false);
             jogarDeNovo.setForeground(new Color(9, 6, 6));
-            jogarDeNovo.setSize(483, 103);
-            jogarDeNovo.setLocation(118, 25);
             jogarDeNovo.setEnabled(false);
         }
         return jogarDeNovo;
@@ -107,8 +148,6 @@ public class PanelTabela extends JPanel {
                 }
             });
             botao0x0.setForeground(Color.BLACK);
-            botao0x0.setSize(140, 140);
-            botao0x0.setLocation(118, 139);
         }
         return botao0x0;
 
@@ -118,10 +157,12 @@ public class PanelTabela extends JPanel {
 
         if (botao0x1 == null) {
             botao0x1 = new JButton();
+            botao0x1.addActionListener(new ActionListener() {
+            	public void actionPerformed(ActionEvent e) {
+            	}
+            });
             botao0x1.setForeground(Color.BLACK);
             botao0x1.setFont(new Font("Comic Sans MS", Font.PLAIN, 70));
-            botao0x1.setSize(140, 140);
-            botao0x1.setLocation(286, 139);
         }
         return botao0x1;
 
@@ -132,8 +173,6 @@ public class PanelTabela extends JPanel {
         if (botao0x2 == null) {
             botao0x2 = new JButton();
             botao0x2.setFont(new Font("Comic Sans MS", Font.PLAIN, 70));
-            botao0x2.setSize(140, 140);
-            botao0x2.setLocation(461, 139);
         }
         return botao0x2;
 
@@ -144,8 +183,6 @@ public class PanelTabela extends JPanel {
         if (botao1x0 == null) {
             botao1x0 = new JButton();
             botao1x0.setFont(new Font("Comic Sans MS", Font.PLAIN, 70));
-            botao1x0.setSize(140, 140);
-            botao1x0.setLocation(118, 301);
         }
         return botao1x0;
 
@@ -156,8 +193,6 @@ public class PanelTabela extends JPanel {
         if (botao1x1 == null) {
             botao1x1 = new JButton();
             botao1x1.setFont(new Font("Comic Sans MS", Font.PLAIN, 70));
-            botao1x1.setSize(140, 140);
-            botao1x1.setLocation(286, 301);
         }
         return botao1x1;
 
@@ -168,8 +203,6 @@ public class PanelTabela extends JPanel {
         if (botao1x2 == null) {
             botao1x2 = new JButton();
             botao1x2.setFont(new Font("Comic Sans MS", Font.PLAIN, 70));
-            botao1x2.setSize(140, 140);
-            botao1x2.setLocation(461, 301);
         }
         return botao1x2;
 
@@ -180,8 +213,6 @@ public class PanelTabela extends JPanel {
         if (botao2x0 == null) {
             botao2x0 = new JButton();
             botao2x0.setFont(new Font("Comic Sans MS", Font.PLAIN, 70));
-            botao2x0.setSize(140, 140);
-            botao2x0.setLocation(118, 466);
         }
         return botao2x0;
 
@@ -192,8 +223,6 @@ public class PanelTabela extends JPanel {
         if (botao2x1 == null) {
             botao2x1 = new JButton();
             botao2x1.setFont(new Font("Comic Sans MS", Font.PLAIN, 70));
-            botao2x1.setSize(140, 140);
-            botao2x1.setLocation(286, 466);
         }
         return botao2x1;
 
@@ -204,8 +233,6 @@ public class PanelTabela extends JPanel {
         if (botao2x2 == null) {
             botao2x2 = new JButton();
             botao2x2.setFont(new Font("Comic Sans MS", Font.PLAIN, 70));
-            botao2x2.setSize(140, 140);
-            botao2x2.setLocation(461, 466);
         }
         return botao2x2;
     }
